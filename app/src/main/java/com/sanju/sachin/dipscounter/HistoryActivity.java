ce08d6ce1b6b980a86c1e4a490aca6f85236f059
sanjay <kochrekarsanjay87@gmail.com>
2019-01-14 13:28:09 +0530

    Updated History activity

--- app/src/main/java/com/sanju/sachin/dipscounter/HistoryActivity.java
+++ app/src/main/java/com/sanju/sachin/dipscounter/HistoryActivity.java
@@ -7,7 +7,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
